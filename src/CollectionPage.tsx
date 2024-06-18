a60db5bb429d57a53de79348743f4f604a2cd68b
Neuron-project <neuron.project@yandex.ru>
2024-06-18 14:01:46 +0300

    modified:   src/CollectionPage.tsx

--- src/CollectionPage.tsx
+++ src/CollectionPage.tsx
@@ -4,14 +4,13 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import styled from "styled-components";
 import "@twa-dev/sdk";
 import BottomNavBar from "./BottomNavBar";
-
 const StyledApp = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  padding: 20px 20px;
+  padding: 10px 10px; /* reduce padding to 10px */
   overflow: hidden;
   background-color: #fff;
   display: flex;
@@ -20,41 +19,44 @@ const StyledApp = styled.div`
 
 const AppContainer = styled.div`
   max-width: 900px;
-  margin: 0 auto; /* Center the content */
+  margin: 0 auto; /* remove left margin */
+  padding: 0 10px; /* reduce padding to 10px */
   display: flex;
   flex-direction: column; /* Arrange items vertically */
   gap: 10px;
-  padding: 0 10px; /* Reduce left and right padding to 10px */
 `;
 
 const CollectionGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(50px, 1fr)); /* Reduce card width to 50px */
+  grid-template-columns: repeat(auto-fit, minmax(33.33%, 1fr)); /* reduce width to 1/3 of original size */
   gap: 10px;
 `;
 
 const CollectionItem = styled.div`
   background-color: #f7f7f7;
   border-radius: 10px;
-  padding: 5px; /* Reduce padding to 5px */
+  padding: 10px;
   text-align: center;
+  width: 33.33%; /* reduce width to 1/3 of original size */
+  height: 100px; /* reduce height to 1/3 of original size */
 `;
 
 const ImageContainer = styled.div`
   width: 100%;
-  height: 75px; /* Reduce image height to 75px */
+  height: 50px; /* reduce height to 1/3 of original size */
   overflow: hidden; /* Hide overflow to create a clean cut */
   border-radius: 8px;
 `;
 
 const CollectionImage = styled.img`
-  width: 100%;
-  height: 100%;
+  width: 50px; /* reduce width to 1/3 of original size */
+  height: 50px; /* reduce height to 1/3 of original size */
   object-fit: cover; /* Scale image to fit container */
 `;
 
 const CollectionText = styled.div`
-  margin-top: 5px; /* Reduce margin to 5px */
+  margin-top: 10px;
+  font-size: 10px; /* reduce font size from default */
 `;
 
 const FlexBoxRow = styled.div`
