ebe3da10b56bef73adabe1ba86dac4d405c0133a
Neuron-project <neuron.project@yandex.ru>
2024-07-02 11:31:48 +0300

    modified:   src/CollectionPage.tsx

--- src/CollectionPage.tsx
+++ src/CollectionPage.tsx
@@ -5,12 +5,7 @@ import styled from "styled-components";
 import "@twa-dev/sdk";
 import BottomNavBar from "./BottomNavBar";
 import AppIcon from "./components/styled/AppIcon.png";
-import  LazyLoad  from "react-lazy-load";
-
-
-
-
-
+import LazyLoad from "react-lazy-load";
 
 const StyledApp = styled.div`
   position: fixed;
@@ -32,11 +27,20 @@ const AppContainer = styled.div`
   padding: 0 10px;
 `;
 
+const CollectionGridContainer = styled.div`
+  overflow-y: scroll; /* Change from auto to scroll */
+  -webkit-scrollbar: none; /* Add this to hide the scrollbar in WebKit browsers */
+  -moz-scrollbar: none; /* Add this to hide the scrollbar in Mozilla browsers */
+  scrollbar-width: none; /* Add this to hide the scrollbar in modern browsers */
+  height: 70vh; /* Keep the fixed height */
+  padding: 10px;
+`;
+
 const CollectionGrid = styled.div`
   display: grid;
-   grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 10px;
- `;
+`;
 
 const CollectionItem = styled.div`
   background-color: #f7f7f7;
@@ -59,9 +63,9 @@ const CollectionImage = styled.img`
 `;
 
 const CollectionText = styled.div`
-font-size: 5px;
-
+  font-size: 5px;
 `;
+
 const FlexBoxRow = styled.div`
   display: flex;
   flex-direction: row;
@@ -69,8 +73,7 @@ const FlexBoxRow = styled.div`
   gap: 10px;
 `;
 
-
-const imageCache: {[key: string]: any } = {};
+const imageCache: { [key: string]: any } = {};
 
 const getImageUrl = (imageKey: string) => {
   return new URL(`./components/styled/${imageKey}`, import.meta.url).href;
@@ -93,7 +96,7 @@ const loadCollectionData = async () => {
   }
 };
 
-const collectionData: { id: number; image: any; number: string; }[] = [];
+const collectionData: { id: number; image: any; number: string }[] = [];
 
 function CollectionPage() {
   const [showCollection, setShowCollection] = useState(false);
@@ -104,55 +107,63 @@ function CollectionPage() {
     });
   }, []);
 
-
-
   return (
     <StyledApp>
-      <FlexBoxRow style={{
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        padding: '8px 16px',
-        backgroundColor: '#f7f7f7',
-        height: 40,
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        borderBottomLeftRadius: 10,
-        borderBottomRightRadius: 10
-      }}>
-        <FlexBoxRow style={{ justifyContent: 'flex-start' }}>
-          <img src={AppIcon} alt="Neuron Icon" style={{
-            width: 25,
-            height: 25
-          }} />
-          <h2 style={{
-            color: '#666', // серый цвет текста
-            background: 'linear-gradient(to right, #666, #999)', // градиент для текста
-            WebkitBackgroundClip: 'text', // для корректного отображения градиента в Chrome
-            WebkitTextFillColor: 'transparent' // для корректного отображения градиента в Chrome
-          }}>
+      <FlexBoxRow
+        style={{
+          justifyContent: "flex-start",
+          alignItems: "center",
+          padding: "8px 16px",
+          backgroundColor: "#f7f7f7",
+          height: 40,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          borderBottomLeftRadius: 10,
+          borderBottomRightRadius: 10,
+        }}
+      >
+        <FlexBoxRow style={{ justifyContent: "flex-start" }}>
+          <img
+            src={AppIcon}
+            alt="Neuron Icon"
+            style={{
+              width: 25,
+              height: 25,
+            }}
+          />
+          <h2
+            style={{
+              color: "#666", // серый цвет текста
+              background: "linear-gradient(to right, #666, #999)", // градиент для текста
+              WebkitBackgroundClip: "text", // для корректного отображения градиента в Chrome
+              WebkitTextFillColor: "transparent", // для корректного отображения градиента в Chrome
+            }}
+          >
             NEURON
           </h2>
         </FlexBoxRow>
-        <TonConnectButton style={{ marginLeft: 'auto' }} /> {/* Используем стилизованную кнопку TonConnect */}
+        <TonConnectButton style={{ marginLeft: "auto" }} /> {/* Используем стилизованную кнопку TonConnect */}
       </FlexBoxRow>
       {showCollection && (
         <AppContainer style={{ marginTop: 60, width: "85vw" }}>
-         <CollectionGrid>
-  {collectionData.map((item) => (
-  <CollectionItem key={item.id}>
-  <ImageContainer>
-    <LazyLoad>
-      <CollectionImage src={item.image} />
-    </LazyLoad>
-  </ImageContainer>
-  <CollectionText>
-    <p>{item.number}</p>
-  </CollectionText>
-</CollectionItem>
-  ))}
-</CollectionGrid>
+          <CollectionGridContainer>
+            <CollectionGrid>
+              {collectionData.map((item) => (
+                <CollectionItem key={item.id}>
+                  <ImageContainer>
+                    <LazyLoad>
+                      <CollectionImage src={item.image} />
+                    </LazyLoad>
+                  </ImageContainer>
+                  <CollectionText>
+                    <p>{item.number}</p>
+                  </CollectionText>
+                </CollectionItem>
+              ))}
+            </CollectionGrid>
+          </CollectionGridContainer>
         </AppContainer>
       )}
       <BottomNavBar />
