362e084fd7ad94fb1f22794d25fc35f8311061ff
Erghauzen <erghauzenderliero@gmail.com>
2024-07-06 17:47:49 +0300

    "Removed overflow-y: hidden from index.html and made minor styling changes to CollectionPage.tsx"

--- src/CollectionPage.tsx
+++ src/CollectionPage.tsx
@@ -15,6 +15,7 @@ const StyledApp = styled.div`
   height: 100vh;
   padding: 20px 20px;
   overflow: hidden;
+  padding: 0 10px;
   background-color: #fff;
   overflow-y: auto; /* Add this to enable scrolling within the app */
 `;
