20552cd81b83155a35c232617d39f665019945d6
Neuron-project <neuron.project@yandex.ru>
2024-07-06 12:16:24 +0300

    "Removed App import and AppFallback component, updated router configuration to use children property"

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,22 +1,14 @@
-import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import CollectionPage from './CollectionPage';
 import HomePage from './HomePage';
-import App from './App';
-
-const AppFallback = () => {
-  return <App />;
-};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
-    errorElement: <AppFallback />, // Add errorElement property
-  },
-  {
-    path: '/collection',
-    element: <CollectionPage />,
-    errorElement: <AppFallback />, // Add errorElement property
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'collection', element: <CollectionPage /> },
+    ],
   },
 ]);
 
