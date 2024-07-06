bbd1c79c80de7db714ad91ff24d4233315aebfbb
Neuron-project <neuron.project@yandex.ru>
2024-07-06 11:48:34 +0300

    modified:   src/Routes.tsx 	new file:   vercel.json

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,13 +3,10 @@ import CollectionPage from './CollectionPage';
 import HomePage from './HomePage';
 import App from './App';
 
-
-
-
 const router = createBrowserRouter([
-    { path: `/`, element: <HomePage /> },
-    { path: `/collection`, element: <CollectionPage /> },
-  ]);
-  
-  export default router;
-  
+  { path: `/`, element: <HomePage /> },
+  { path: `/collection`, element: <CollectionPage /> },
+  { path: '/*', element: <App /> }, // Add this catch-all route
+]);
+
+export default router;
