fb7fccfbfb3f9ff8910d5149c82319cd6b2f3865
Neuron-project <neuron.project@yandex.ru>
2024-07-06 18:02:12 +0300

    modified:   src/components/styled/styled.tsx

--- src/components/styled/styled.tsx
+++ src/components/styled/styled.tsx
@@ -1,18 +1,6 @@
 import styled from "styled-components";
 
 
-export const Button = styled.button`
-  background-color: ${(props) =>
-    props.disabled ? "#6e6e6e" : "var(--tg-theme-button-color)"};
-  border: 0;
-  border-radius: 8px;
-  padding: 10px 20px;
-  color: var(--tg-theme-button-text-color);
-  font-weight: 700;
-  cursor: pointer;
-  pointer-events: ${(props) => (props.disabled ? "none" : "inherit")};
-`;
-
 export const Ellipsis = styled.div`
   text-overflow: ellipsis;
   overflow: hidden;
